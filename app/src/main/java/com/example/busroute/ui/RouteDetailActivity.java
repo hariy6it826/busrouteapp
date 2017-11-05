ee6644fa98c246c79281a734cd3f7358b55268d3
dasari <dasari@hubblehome.com>
2017-11-06 01:59:45 +0530

    Implemented accessibility based on the data

--- app/src/main/java/com/example/busroute/ui/RouteDetailActivity.java
+++ app/src/main/java/com/example/busroute/ui/RouteDetailActivity.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -23,17 +24,23 @@ public class RouteDetailActivity extends AppCompatActivity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_route_detail);
+
         RouteModel.Routes selectedRoute = getIntent().getParcelableExtra("ROUTE_INFO");
-        Parcelable [] parcelableStops = getIntent().getParcelableArrayExtra("STOPS_INFO");
-        RouteModel.Stops [] stops = new RouteModel.Stops[parcelableStops.length];
-        for(int i = 0; i < parcelableStops.length; i++){
-            stops[i] = (RouteModel.Stops)parcelableStops[i];
+        Parcelable[] parcelableStops = getIntent().getParcelableArrayExtra("STOPS_INFO");
+        RouteModel.Stops[] stops = new RouteModel.Stops[parcelableStops.length];
+        for (int i = 0; i < parcelableStops.length; i++) {
+            stops[i] = (RouteModel.Stops) parcelableStops[i];
         }
 
         TextView routeName = findViewById(R.id.route_name);
         TextView routeDesc = findViewById(R.id.route_description);
         ImageView routeImage = findViewById(R.id.route_image);
-
+        ImageView accessibilityView = findViewById(R.id.accessibility);
+        if(selectedRoute.getAccessible().equals("true")){
+            accessibilityView.setVisibility(View.VISIBLE);
+        }else{
+            accessibilityView.setVisibility(View.GONE);
+        }
         routeName.setText(selectedRoute.getName());
         routeDesc.setText(selectedRoute.getDescription());
         Picasso.with(this).load(selectedRoute.getImage()).into(routeImage);
